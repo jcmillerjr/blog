afa7baff67c84fa66a443243d3448e1d6855576f
CatalogMaster <jcmillerjr@me.com>
2019-10-19 08:43:04 -0400

    update with CSS

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,6 @@ from django.views.generic import ListView
 from .models    import Post
 
 # Create your views here.
-class BlogListVIew(ListView):
+class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
